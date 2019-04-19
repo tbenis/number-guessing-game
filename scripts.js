f6bfb78fb136decd584ee5fa905947b7c56abc0b
tbenis <will.jenifer@yahoo.com>
2019-04-18 20:15:08 -0400

    Simplified code. added heading

--- scripts.js
+++ scripts.js
@@ -3,22 +3,25 @@ function myGuess(){
     var number = Math.ceil(Math.random() * 10) + 1;
     //User's guess
     var yourNumber = document.getElementsByName("userInput");
+    var resultComment = document.getElementsByClassName('resultComment');
+    var iconChange = document.getElementsByClassName('icon-change');
+    var error_m = document.getElementsByClassName('error-m');
     console.log(yourNumber[0].value);
     //if no guess/ valid number input
     if (!yourNumber[0].value) {
-        document.getElementsByClassName('resultComment')[0].innerHTML = ""
-        document.getElementsByClassName('icon-change')[0].innerHTML = "<i class=\"fas fa-exclamation-triangle\"></i> ";
-        document.getElementsByClassName('error-m')[0].innerHTML = "Invalid value (HINT: Please enter a number)";
+        resultComment[0].innerHTML = ""
+        iconChange[0].innerHTML = "<i class=\"fas fa-exclamation-triangle\"></i> ";
+        error_m[0].innerHTML = "Invalid value (HINT: Please enter a number)";
         yourNumber[0].classList.add("is-danger");
     } else if (yourNumber[0].value == number){ //if Match
-        document.getElementsByClassName('resultComment')[0].innerHTML = "<h2>Yay! Way to go!</h2>";
-        document.getElementsByClassName('icon-change')[0].innerHTML = "";
-        document.getElementsByClassName('error-m')[0].innerHTML = "";
+        resultComment[0].innerHTML = "<h2>Yay! Way to go!</h2>";
+        iconChange[0].innerHTML = "";
+        error_m.innerHTML = "";
         yourNumber[0].classList.remove("is-danger");
     } else { // If no Match
-        document.getElementsByClassName('resultComment')[0].innerHTML = '<h3>Sorry That\'s not correct. The answer is: <strong>'+number+'</strong> </h3>';
-        document.getElementsByClassName('icon-change')[0].innerHTML = "";
-        document.getElementsByClassName('error-m')[0].innerHTML = "";
+        resultComment[0].innerHTML = '<h3>Sorry That\'s not correct. The answer is: <strong>'+number+'</strong> </h3>';
+        iconChange[0].innerHTML = "";
+        error_m[0].innerHTML = "";
         yourNumber[0].classList.remove("is-danger");
     }
     //Clear inout field regardless
